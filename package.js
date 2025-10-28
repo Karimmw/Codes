db36a69ac287bab6c3ea9023ccaabc67607f738e
Karimmw <131699160+Karimmw@users.noreply.github.com>
2025-10-28 15:20:01 +0300

    Change package name and simplify package.json
    Updated package name and removed unnecessary fields.

--- package.js
+++ package.js
@@ -1,12 +1,5 @@
 {
-  "name": "telegram-signal-bot",
+  "name": "signal-bot",
   "version": "1.0.0",
-  "description": "Instant Telegram signal processor",
-  "scripts": {
-    "dev": "vercel dev"
-  },
-  "dependencies": {},
-  "devDependencies": {
-    "vercel": "^28.0.0"
-  }
+  "type": "module"
 }
