63d9261ad9b9abdb58904387d85d690069307f93
Karimmw <131699160+Karimmw@users.noreply.github.com>
2025-10-28 15:55:17 +0300

    Update TARGET_PATH to include 'codes/' directory

--- api/telegram.js
+++ api/telegram.js
@@ -1,7 +1,7 @@
 // api/telegram.js
 const GITHUB_OWNER = "Karimmw";
 const GITHUB_REPO = "Codes";
-const TARGET_PATH = "Sigs.json";
+const TARGET_PATH = "codes/Sigs.json";
 const BRANCH = "main";
 
 export default async function handler(req, res) {
