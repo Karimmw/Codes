6a7caa578d88d1246fb4ccf6602d34d478becf66
Karimmw <131699160+Karimmw@users.noreply.github.com>
2025-10-28 07:19:16 +0300

    Initialize Package.js for telegram-signal-bot

---
+++ Package.js
@@ -0,0 +1,13 @@
+{
+  "name": "telegram-signal-bot",
+  "version": "1.0.0",
+  "description": "Instant Telegram signal processor",
+  "main": "index.js",
+  "scripts": {
+    "dev": "vercel dev"
+  },
+  "dependencies": {},
+  "devDependencies": {
+    "vercel": "^28.0.0"
+  }
+}
